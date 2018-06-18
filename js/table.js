708442a54f5da8e00e90f6e0bf03e6702ee29dad
invertisment <visiskaj.slaptaj@gmail.com>
2018-06-18 21:33:13 +0300

    Clickable urls

--- js/table.js
+++ js/table.js
@@ -1,3 +1,24 @@
+
+function getLocation(url) {
+    var l = document.createElement("a");
+    l.href = url;
+    return l.hostname.replace(/www[.]/gi, '')
+        + l.pathname.replace(/\/$/gi, '') + "</a>";
+};
+
+function singleLinkFormatter(url) {
+    if (url == null) {
+        return;
+    }
+    return "<a class='link' href='" + url + "'>"
+        + getLocation(url) + "</a>";
+}
+
+function multilinkFormatter(urls) {
+    var aa = urls.map(singleLinkFormatter);
+    return aa.reduce((acc, a) => acc + a, "");
+}
+
 fetch("https://raw.githubusercontent.com/Invertisment/eos-data/master/data.json")
     .then(res => res.json())
     .then((out) => {
@@ -18,9 +39,10 @@ fetch("https://raw.githubusercontent.com/Invertisment/eos-data/master/data.json"
                             rowspan: 2,
                         },{
                             field: 'bp-header.url',
-                            title: 'link',
+                            title: 'Link',
                             sortable: true,
                             rowspan: 2,
+                            formatter: singleLinkFormatter,
                         }, {
                             title: 'Webpage content summary',
                             colspan: 3,
@@ -45,22 +67,37 @@ fetch("https://raw.githubusercontent.com/Invertisment/eos-data/master/data.json"
                             sortable: true,
                             align: 'right',
                         }
-
                         , {
                             title: 'Relative link count',
                             field: 'analyzed-urls.relative-links-count',
                             sortable: true,
                             align: 'right',
-                        }, {
+                        },
+                        //{
+                        //    title: 'Social links count',
+                        //    field: 'analyzed-urls.social-links-count',
+                        //    sortable: true,
+                        //    align: 'right',
+                        //}, {
+                        //    title: 'Popularity boost links count',
+                        //    field: 'analyzed-urls.popularity-boosters-count',
+                        //    sortable: true,
+                        //    align: 'right',
+                        //}
+
+
+                        {
                             title: 'Social links',
                             field: 'analyzed-urls.social-links',
                             sortable: true,
                             align: 'right',
+                            formatter: multilinkFormatter,
                         }, {
                             title: 'Popularity boost links',
                             field: 'analyzed-urls.popularity-boosters',
                             sortable: true,
                             align: 'right',
+                            formatter: multilinkFormatter,
                         }
 
                     ]]
