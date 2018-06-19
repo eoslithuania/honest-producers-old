a9ade51d2a257ea7410ee9541370a2bd33e04d54
invertisment <visiskaj.slaptaj@gmail.com>
2018-06-19 19:23:33 +0300

    Site simplification

--- js/table.js
+++ js/table.js
@@ -9,12 +9,19 @@ function getLocation(url) {
         + l.pathname.replace(/\/$/gi, '') + "</a>";
 };
 
-function singleLinkFormatter(url) {
+function monospaceText(text) {
+    return "<span class='monospace large-font'>" + text + "</span>"
+}
+
+function createLink(url, title) {
     if (url == null || url.length == 0) {
         return;
     }
-    return "<a class='link' href='" + url + "'>"
-        + getLocation(url) + "</a>";
+    return "<a class='link' href='" + url + "'>" + title + "</a>";
+}
+
+function singleLinkFormatter(url) {
+    return createLink(url, getLocation(url));
 }
 
 function multilinkFormatter(urls) {
@@ -22,89 +29,118 @@ function multilinkFormatter(urls) {
     return aa.reduce((acc, a) => acc + a, "");
 }
 
+function titleFormatter(title, dataRow) {
+    var url = dataRow["bp-header"]["prepared-url"];
+    var monospaceTitle = monospaceText(title);
+    if (url == null) {
+        return monospaceTitle;
+    }
+    return createLink(url, monospaceTitle);
+}
+
+var mutableDataRef = null;
+
+function openMoreInfoDialog(dataRowId) {
+    dataRow = mutableDataRef["block-producers"][dataRowId];
+    console.log("Works", dataRow);
+    $("#myModal .modal-title").html(dataRow["bp-header"]["owner"])
+    $("#myModal").modal();
+}
+
+function moreInfoFormatter(idk, dataRow, dataId) {
+    return "<button \
+        class='btn btn-info btn-sm' \
+        onClick='openMoreInfoDialog(" + dataId + ")'>More info</button>";
+}
+
 fetch("./data/data.json")
     .then(res => res.json())
     .then((out) => {
+        mutableDataRef = out;
         $(function () {
             $('#table').bootstrapTable({
                 data: out['block-producers'],
-                search: true,
-                pagination: true,
-                buttonsClass: 'primary',
-                minimumCountColumns: 2,
                 pageList:[10, 50, 100, "All"],
                 columns: [
                     [
+                        {
+                            title: 'Rating',
+                            sortable: true,
+                            rowspan: 2,
+                        },
                         {
                             field: 'bp-header.owner',
                             title: 'BP name',
                             sortable: true,
                             rowspan: 2,
-                        },{
-                            field: 'bp-header.prepared-url',
-                            title: 'Link',
-                            sortable: true,
-                            rowspan: 2,
-                            formatter: singleLinkFormatter,
-                        }, {
-                            title: 'Webpage content summary',
-                            colspan: 3,
-                        },{
-                            title: 'Analyzed urls',
-                            colspan: 4,
-                        },],
-                    [
-                        {
-                            title: 'Image count',
-                            field: 'summary.img-count',
-                            sortable: true,
-                            align: 'right',
-                        }, {
-                            title: 'Paragraph count',
-                            field: 'summary.p-count',
-                            sortable: true,
-                            align: 'right',
-                        }, {
-                            title: 'Link count',
-                            field: 'summary.a-count',
-                            sortable: true,
-                            align: 'right',
-                        }
-                        , {
-                            title: 'Relative link count',
-                            field: 'analyzed-urls.relative-links-count',
-                            sortable: true,
-                            align: 'right',
+                            formatter: titleFormatter,
                         },
                         {
-                            title: 'Social links count',
+                            title: 'Webpage content summary',
+                            colspan: 3,
+                        },
+                    ],
+                    [
+                        //{
+                        //    title: 'Image count',
+                        //    field: 'summary.img-count',
+                        //    sortable: true,
+                        //    align: 'right',
+                        //}, {
+                        //    title: 'Paragraph count',
+                        //    field: 'summary.p-count',
+                        //    sortable: true,
+                        //    align: 'right',
+                        //}, {
+                        //    title: 'Link count',
+                        //    field: 'summary.a-count',
+                        //    sortable: true,
+                        //    align: 'right',
+                        //}
+                        //, {
+                        //    title: 'Relative link count',
+                        //    field: 'analyzed-urls.relative-links-count',
+                        //    sortable: true,
+                        //    align: 'right',
+                        //},
+                        {
+                            title: 'Social links',
                             field: 'analyzed-urls.social-links-count',
                             sortable: true,
                             align: 'right',
                         }, {
-                            title: 'Popularity boost links count',
+                            title: 'Popularity boost links',
                             field: 'analyzed-urls.popularity-boosters-count',
                             sortable: true,
                             align: 'right',
                         },
+                        //{
+                        //    title: 'Social links',
+                        //    field: 'analyzed-urls.social-links',
+                        //    sortable: true,
+                        //    align: 'right',
+                        //    formatter: multilinkFormatter,
+                        //}, {
+                        //    title: 'Popularity boost links',
+                        //    field: 'analyzed-urls.popularity-boosters',
+                        //    sortable: true,
+                        //    align: 'right',
+                        //    formatter: multilinkFormatter,
+                        //},
+                        //{
+                        //    title: 'err',
+                        //    field: 'http-error',
+                        //    sortable: true,
+                        //    align: 'right',
+                        //},
+                        //{
+                        //    title: 'More',
+                        //    field: 'http-error',
+                        //    sortable: true,
+                        //    align: 'right',
+                        //},
                         {
-                            title: 'Social links',
-                            field: 'analyzed-urls.social-links',
-                            sortable: true,
-                            align: 'right',
-                            formatter: multilinkFormatter,
-                        }, {
-                            title: 'Popularity boost links',
-                            field: 'analyzed-urls.popularity-boosters',
-                            sortable: true,
-                            align: 'right',
-                            formatter: multilinkFormatter,
-                        },
-                        {
-                            title: 'err',
-                            field: 'http-error',
-                            sortable: true,
-                            align: 'right',
+                            formatter: moreInfoFormatter,
                         },
 
                     ]]
